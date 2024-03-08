01cd6a3a913479fb50257221b5d0ea7237ffcd1f
ztk37 <ztk37.contact@gmail.com>
2024-03-08 20:31:14 +0200

    chore: Setup shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from "node:path";
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => ({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
   clearScreen: false,
   server: {
     port: 1420,
